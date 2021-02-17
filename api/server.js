6f18942540d1a49370da8e3118fd43bf84122734
chqiu6 <chucky-qiu@lambdastudents.com>
2021-02-16 22:03:15 -0500

    just started will finish later

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const postsRouter = require("./posts/posts-router");
+const usersRouter = require("./users/users-router");
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json());
 // global middlewares and routes need to be connected here
 
 server.get('/', (req, res) => {

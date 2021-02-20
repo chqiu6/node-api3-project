a20b5595ee18bcc761e8d1ced5f84cd9efcb7574
chqiu6 <chucky-qiu@lambdastudents.com>
2021-02-19 19:56:00 -0500

    testing if logger is the problem for heroku deploy

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express();
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
 // global middlewares and routes need to be connected here
-server.use(logger);
+// server.use(logger);
 server.use(postsRouter);
 server.use(usersRouter);
 
